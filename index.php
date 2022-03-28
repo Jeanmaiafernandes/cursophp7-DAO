2325ae9792786b64afa4140ed84b2207ed275435
Jeanmaiafernandes <jeanmaiafernandes@gmail.com>
2022-03-28 12:56:45 -0300

    comando delete

--- index.php
+++ index.php
@@ -45,12 +45,37 @@ $usuario->login ("José", "1234567890");
 
 echo $usuario;
 */
-
+//=================================================================//
+             //Criando um novo usuário
+/*
 $aluno = new Usuario("aluno", "@lun0");
 
 $aluno->insert();
 
 echo $aluno;
+*/
 
+//=================================================================//
+                // Alterar um usuário
+/*
+$usuario = new Usuario();
+
+$usuario->loadById(8);
+
+$usuario->update("Roberto", "456899");
+
+echo $usuario;
+*/
+
+//=================================================================//
+
+
+$usuario = new Usuario();
+
+$usuario->loadById(9);
+
+$usuario->delete();
+
+echo $usuario
 
 ?>
